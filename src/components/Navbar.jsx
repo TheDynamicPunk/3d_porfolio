0517ea20a3a71fb53ef1d56d4e2e86331e582c86
theDynamicPunk <ashutoshgu18@gmail.com>
2023-12-27 21:25:35 +0530

    update in page links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
               font-medium cursor-pointer`}
               onClick={() => setActive(link.title)}
               >
-              <a href={`${link.id}`}>{link.title}</a>
+              <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
@@ -63,7 +63,7 @@ const Navbar = () => {
                   setActive(link.title)
                   }}
                 >
-                <a href={`${link.id}`}>{link.title}</a>
+                <a href={`#${link.id}`}>{link.title}</a>
               </li>
             ))}
           </ul>
